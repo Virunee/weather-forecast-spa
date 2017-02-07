3cb8e469e838474ee506927bca4fe9de1e11fc77
Virunee <viravongv@gmail.com>
2017-02-07 21:55:48 +0000

    Added cityService
    Data bound input to forecasting page

--- app.js
+++ app.js
@@ -16,15 +16,28 @@ weatherApp.config(function ($routeProvider) {
     })
 
 });
+//SERVICES
+
+weatherApp.service('cityService', function() {
+
+  this.city = "London, GB";
+
+});
 
 //CONTROLLERS
 
-weatherApp.controller('mainController', ['$scope', function($scope) {
+weatherApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService) {
 
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function () {
+    cityService.city = $scope.city;
+  });
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope','cityService', function($scope, cityService) {
 
+  $scope.city = cityService.city;
 
 }]);
