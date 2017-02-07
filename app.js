5e0e960b154b95a9813824142a8991affada7a32
Virunee <viravongv@gmail.com>
2017-02-07 23:14:19 +0000

    Implemented calls to API
    Initial API calls

--- app.js
+++ app.js
@@ -36,8 +36,21 @@ weatherApp.controller('mainController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope','cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope','$resource','cityService', function($scope, $resource, cityService) {
 
   $scope.city = cityService.city;
+
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=cb0d63f0af9b39ee91a56e99c1cbc268", {
+    callback: "JSON_CALLBACK"}, {get: {method: "JSONP" }});
+
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt:2 });
+
+    $scope.convertToCelsius = function(degK) {
+      return Math.round(degK - 273.15);
+    };
+
+    $scope.convertToDate = function(dt) {
+      return new Date(dt*1000);
+    }
 
 }]);
