60d2fa39b48f18b15f83f22663ce8eee62c2371a
Virunee <viravongv@gmail.com>
2017-02-07 21:24:51 +0000

    Add two pages

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 //MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//Routing
+weatherApp.config(function ($routeProvider) {
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
+    })
+
+    .when('/forecasting', {
+      templateUrl: 'pages/forecasting.html',
+      controller: 'forecastingController'
+    })
+
+});
+
+//CONTROLLERS
+
+weatherApp.controller('mainController', ['$scope', function($scope) {
+
+
+}]);
+
+weatherApp.controller('forecastingController', ['$scope', function($scope) {
+
+
+}]);
